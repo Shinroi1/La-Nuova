33f022de5688fc962cf09acd56d259c868919fd6
Shinroi1 <altoverossam@gmail.com>
2024-04-11 15:55:28 +0800

    Commit changes

--- website/myapp/admin.py
+++ website/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Menu)
+admin.site.register(ReserveTable)
+admin.site.register(ExclusiveReserveTable)
