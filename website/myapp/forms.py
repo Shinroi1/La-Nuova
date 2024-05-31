571c93248c6c0d9e7d209c6a0b24e94d5e786539
Shinroi1 <altoverossam@gmail.com>
2024-05-31 20:32:50 +0800

    Commit Changes

--- website/myapp/forms.py
+++ website/myapp/forms.py
@@ -4,50 +4,121 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.validators import MaxValueValidator
 from django.core.validators import MinValueValidator
-from django_select2.forms import Select2MultipleWidget
+from django.forms import ModelForm
+
+# from django_select2.forms import Select2MultipleWidget
 
 class AdminRegisterForm(UserCreationForm):
+    
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=65)
-    last_name = forms.CharField(max_length=65)
+    
+    first_name = forms.CharField(
+        max_length=65
+        )
+    
+    last_name = forms.CharField(
+        max_length=65
+        )
         
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name','password1', 'password2')
+        fields = ('username', 
+                  'email', 
+                  'first_name', 
+                  'last_name',
+                  'password1', 
+                  'password2'
+                  )
         
 class CustomerForm(forms.ModelForm):
     
-    fullname = forms.CharField(required=True ,max_length=65)
-    address = forms.CharField(required=True, max_length=65)
-    phone = forms.CharField(required=True, max_length=11)
+    fullname = forms.CharField(
+        required=True,
+        max_length=65
+        )
+    
+    address = forms.CharField(
+        required=True, 
+        max_length=65
+        )
+    phone = forms.CharField(
+        required=True, 
+        max_length=11
+        )
     
     class Meta:
         model = Customer
-        fields = ('fullname', 'address' , 'phone')
+        fields = ('fullname', 
+                  'address' , 
+                  'phone'
+                  )
         
 class MenuForm(forms.ModelForm):
     
-    dish_name = forms.CharField(required=True ,max_length=65)
-    category = forms.CharField(required=True, max_length=65)
-    sub_category = forms.CharField(required=True, max_length=65)
-    ingredients = forms.CharField(required=True, max_length=200)
-    price = forms.DecimalField(required=True, decimal_places=2, max_digits=10)
+    dish_name = forms.CharField(
+        required=True,
+        max_length=65
+        )
+    category = forms.CharField(
+        required=True, 
+        max_length=65
+        )
+    sub_category = forms.CharField(
+        required=True, 
+        max_length=65
+        )
+    ingredients = forms.CharField(
+        required=True, 
+        max_length=200
+        )
+    
+    price = forms.DecimalField(
+        required=True, 
+        decimal_places=2, 
+        max_digits=10
+        )
     
     class Meta:
         model = Menu
-        fields = ('dish_name', 'category', 'sub_category' ,'ingredients', 'price')
+        fields = ('dish_name', 
+                  'category', 
+                  'sub_category',
+                  'ingredients', 
+                  'price'
+                  )
         
 class ReservationForm(forms.ModelForm):
-    fullname = forms.ModelChoiceField(queryset=Customer.objects.all(), required=True)
-    table_number = forms.CharField(required=True, max_length=20, help_text="Maximum of tables are 11")  
-    party_size = forms.IntegerField(required=True, validators=[MinValueValidator(1), MaxValueValidator(12)], help_text="Maximum of 12 party size")
+    
+    fullname = forms.ModelChoiceField(
+        queryset=Customer.objects.all(), 
+        required=True
+        )
+    
+    table_number = forms.CharField(
+        required=True, 
+        max_length=20, 
+        help_text="Maximum of tables are 11"
+        )
+      
+    party_size = forms.IntegerField(
+        required=True, 
+        validators=[MinValueValidator(1), MaxValueValidator(12)], 
+        help_text="Maximum of 12 party size"
+        )
     dish_name = forms.ModelMultipleChoiceField(
         queryset=Menu.objects.all(),
-        widget=Select2MultipleWidget,  # Use Select2MultipleWidget here
+#        widget=Select2MultipleWidget,  # Use Select2MultipleWidget here
         required=False
     )
-    total_price = forms.DecimalField(max_digits=10, decimal_places=2, required=False)    
-    table_status = forms.ChoiceField(choices=ReserveTable.TABLESTATUS, required=True)
+    total_price = forms.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        required=False)    
+    
+    table_status = forms.ChoiceField(
+        choices=ReserveTable.TABLESTATUS, 
+        required=True
+        )
     
     def clean(self):
         cleaned_data = super().clean()
@@ -65,21 +136,57 @@ class ReservationForm(forms.ModelForm):
 
     class Meta:
         model = ReserveTable
-        fields = ('fullname', 'table_number', 'party_size', 'dish_name', 'table_status', 'total_price')
+        fields = ('fullname', 
+                  'table_number', 
+                  'party_size', 
+                  'dish_name', 
+                  'table_status', 
+                  'total_price'
+                  )
         
 class ExclusiveReservationForm(forms.ModelForm):
-    fullname = forms.ModelChoiceField(queryset=Customer.objects.all(), required=True)
-    party_size = forms.IntegerField(required=True, validators=[MinValueValidator(1), MaxValueValidator(12)], help_text="Maximum of 12 party size")
+    fullname = forms.ModelChoiceField(
+        queryset=Customer.objects.all(), 
+        required=True
+        )
+    
+    party_size = forms.IntegerField(
+        required=True, 
+        validators=[MinValueValidator(1), MaxValueValidator(12)], 
+        help_text="Maximum of 12 party size"
+        )
+    
     dish_name = forms.ModelMultipleChoiceField(
         queryset=Menu.objects.all(),
-        widget=Select2MultipleWidget,  # Use Select2MultipleWidget here
+#        widget=Select2MultipleWidget,  # Use Select2MultipleWidget here
         required=False
     )
-    total_price = forms.DecimalField(max_digits=10, decimal_places=2, required=False)    
-    table_status = forms.ChoiceField(choices=ReserveTable.TABLESTATUS, required=True)
-    reservation_start_time = forms.TimeField(input_formats=["%I:%M%p"], required=True)
-    reservation_end_time = forms.TimeField(input_formats=["%I:%M%p"], required=True)
-    reservation_duration_hours = forms.IntegerField(required=True, validators=[MinValueValidator(1)], help_text="Maximum of 3 hours")
+    
+    total_price = forms.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        required=False
+        )
+        
+    table_status = forms.ChoiceField(
+        choices=ReserveTable.TABLESTATUS, 
+        required=True
+        )
+    
+    reservation_start_time = forms.TimeField(
+        input_formats=["%I:%M%p"], 
+        required=True
+        )
+    
+    reservation_end_time = forms.TimeField(
+        input_formats=["%I:%M%p"], 
+        required=True
+        )
+    
+    reservation_duration_hours = forms.IntegerField(
+        required=True, validators=[MinValueValidator(1)], 
+        help_text="Maximum of 3 hours"
+        )
 
     def clean(self):
         cleaned_data = super().clean()
@@ -105,4 +212,12 @@ class ExclusiveReservationForm(forms.ModelForm):
     
     class Meta:
         model = ExclusiveReserveTable
-        fields = ('fullname','party_size', 'dish_name', 'total_price','table_status', 'reservation_start_time', 'reservation_end_time','reservation_duration_hours')
+        fields = ('fullname',
+                  'party_size', 
+                  'dish_name', 
+                  'total_price',
+                  'table_status', 
+                  'reservation_start_time', 
+                  'reservation_end_time',
+                  'reservation_duration_hours'
+                  )
--- website/myapp/models.py
+++ website/myapp/models.py
@@ -1,23 +1,62 @@
+from datetime import datetime,timedelta
 from django.db import models
+from uuid import uuid4
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 
+
 # Create your models here.
 class Customer(models.Model):
-    fullname = models.CharField(max_length=200, null=True)
-    address = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=11, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    fullname = models.CharField(
+        max_length=200, 
+        null=True
+        )
+    
+    address = models.CharField(
+        max_length=200, 
+        null=True
+        )
+    
+    phone = models.CharField(
+        max_length=11, 
+        null=True
+        )
+    
+    date_created = models.DateTimeField(
+        auto_now_add=True, 
+        null=True
+        )
     
     def __str__(self):
         return  self.fullname        
 
 class Menu(models.Model):
-    dish_name = models.CharField(max_length=200, null=True)
-    category = models.CharField(max_length=200, null=True)
-    sub_category = models.CharField(max_length=200, default="None")
-    ingredients = models.CharField(max_length=200, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2,null=True)    
+    dish_name = models.CharField(
+        max_length=200, 
+        null=True
+        )
+    
+    category = models.CharField(
+        max_length=200, 
+        null=True
+        )
+    
+    sub_category = models.CharField(
+        max_length=200, 
+        default="None"
+        )
+    
+    ingredients = models.CharField(
+        max_length=200, 
+        null=True
+        )
+    
+    price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2,
+        null=True
+        )    
     
     def __str__(self):
         return f"{self.dish_name} {self.category}"
@@ -29,13 +68,40 @@ class ReserveTable(models.Model):
         ('Completed', 'Completed'), # Means the customer/s is finished using the table
         ('Cancelled', 'Cancelled'),     
         )
-    fullname = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)    
-    table_number = models.CharField(max_length=20, null=True)
-    party_size = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(12)])    
+    fullname = models.ForeignKey(
+        Customer, 
+        on_delete=models.CASCADE, 
+        null=True
+        )
+        
+    table_number = models.CharField(
+        max_length=20, 
+        null=True
+        )
+    
+    party_size = models.IntegerField(
+        default=1, 
+        validators=[MinValueValidator(1), MaxValueValidator(12)]
+        )    
+    
     dish_name = models.ManyToManyField(Menu)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    table_status = models.CharField(max_length=200, null=True, choices=TABLESTATUS)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    total_price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        null=True
+        )
+    
+    table_status = models.CharField(
+        max_length=200, 
+        null=True, 
+        choices=TABLESTATUS
+        )
+    
+    date_created = models.DateTimeField(
+        auto_now_add=True, 
+        null=True
+        )
 
     def __str__(self):
         dish_names = ", ".join(str(menu) for menu in self.dish_name.all())
@@ -43,27 +109,91 @@ class ReserveTable(models.Model):
         return f"{self.table_number} {dish_names}"
     
 class ExclusiveReserveTable(models.Model):
+    
     TABLESTATUS = (
         ('Downpayment', 'Downpayment paid'),
         ('Reserved', 'Reserved for 3 hours'), 
         ('Paid', 'Complete Paid'),
         ('Cancelled', 'Cancelled'),
         )
-    fullname = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
-    party_size = models.IntegerField(null=True, validators=[MinValueValidator(1)])
+    
+    fullname = models.ForeignKey(
+        Customer, 
+        on_delete=models.CASCADE, 
+        null=True
+        )
+    
+    party_size = models.IntegerField(
+        null=True, 
+        validators=[MinValueValidator(1)]
+        )
+    
     dish_name = models.ManyToManyField(Menu)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    table_status = models.CharField(max_length=200, null=True, choices=TABLESTATUS)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    total_price = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        null=True
+        )
+    
+    table_status = models.CharField(
+        max_length=200, 
+        null=True, 
+        choices=TABLESTATUS
+        )
+    
+    date_created = models.DateTimeField(
+        auto_now_add=True, 
+        null=True
+        )
     reservation_start_time = models.TimeField()
+    
     reservation_end_time = models.TimeField()    
-    reservation_duration_hours = models.IntegerField(default=3, validators=[MinValueValidator(1)])    
+    
+    reservation_duration_hours = models.IntegerField(
+        default=3, 
+        validators=[MinValueValidator(1)]
+        )    
     
     def clean(self):
         
-        if self.reservation_duration_hours <= 0:
+        if self.reservation_end_time <= self.reservation_start_time:
             raise ValidationError("Reservation duration must be a positive integer.")
+        
+        start_time = datetime.combine(
+            self.date_created, 
+            self.reservation_start_time
+            ) 
+        
+        end_time = datetime.combine(
+            self.date_created, 
+            self.reservation_end_time
+            )
+        
+        duration_hour = (start_time - end_time).total_seconds() / 3600
+        
+        if duration_hour < self.reservation_duration_hours:
+            raise ValidationError("Reservation duration must be greater than or equal to 3 hours.")
 
     def __str__(self):
         dish_names = ", ".join(str(menu) for menu in self.dish_name.all())
-        return f"{self.dish_name}"
+        return f"{dish_names}"
+
+class Interaction(models.Model):
+    
+    session_id = models.CharField(
+        max_length=50,
+        default=uuid4,
+        unique=True
+        )
+    
+    user_message = models.TextField()
+    
+    bot_response = models.TextField()
+    
+    timestamp = models.DateTimeField(
+        auto_now_add=True
+        )
+    
+    def __str__(self) -> str:
+        return f"Session {self.session_id} - Interaction on {self.timestamp} - Message: {self.user_message[:30]}..."
